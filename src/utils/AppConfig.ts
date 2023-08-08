86b76d1ee6233c76c7116f135d69e42cc95fb49f
Albert Hidalgo <itsalb3rt@gmail.com>
2023-08-07 21:17:37 -0400

    added more app information

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,7 @@
 export const AppConfig = {
   site_name: 'SavyCart',
   title: 'SavyCart',
-  description: 'Create the supermarket list and carry it in your pocket.',
+  description:
+    'Create the grocery list and carry it in your pocket. no more pencil and paper.',
   locale: 'en',
 };
